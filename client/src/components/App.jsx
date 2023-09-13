f885cba2fcf3da4813bc6d3912f12179bb4be94d
Zoe Guidry <oyezoey@gmail.com>
2023-09-12 19:18:54 -0500

    start client frontend

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect, useContext } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import appLogo from './../assets/logo.svg'
+import '../styles/App.css'
+
+import reactLogo from './../assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
